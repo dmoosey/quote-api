9ea88ba64f981a0c0481891018019a26c602650f
dmoosey <56768881+dmoosey@users.noreply.github.com>
2020-12-09 12:18:23 +0000

    add /quotes(?person=) GET route

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,18 @@ router.get('/quotes/random', (req, res) => {
     const randomQuote = getRandomElement(quotes);
     res.send({quote: randomQuote});
 })
+
+router.get('/quotes', (req, res) => {
+    const person = req.query.person;
+    if(person){
+        const newArr = [];
+        for(const quote in quotes){
+            if(quotes[quote].person === person){
+                newArr.push(quotes[quote]);
+            }
+        }
+        res.send({quotes: newArr});
+    }
+    res.send({quotes: quotes});
+})
 module.exports = router;
